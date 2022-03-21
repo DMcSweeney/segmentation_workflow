Mixed precision training - Fix: gradient issues

train.py
@@ -1,10 +1,10 @@
 """
 Main Training script 
 """
-from albumentations.augmentations.transforms import ElasticTransform
+from albumentations.augmentations.geometric.transforms import ElasticTransform
 import torch
 import albumentations as A
-from albumentations.pytorch import ToTensor
+from albumentations.pytorch import ToTensorV2
 from torch.optim import optimizer
 
 from torch.utils.data import DataLoader
@@ -33,14 +33,14 @@ batch_size = int(config['TRAINING']['BatchSize'])
 num_epochs= int(config['TRAINING']['NumEpochs'])
 learning_rate = float(config['TRAINING']['LR'])
 device = f"cuda:{int(config['TRAINING']['GPU'])}"
-
+print('Using device: ', device)
 weight_path = config['TRAINING']['InitWeights']
 
 
 def load_weights(pt_model):
     model = models.segmentation.fcn_resnet101(pretrained=False, num_classes=1)
     #* Load weights
-    pt_dict = torch.load(pt_model)
+    pt_dict = torch.load(pt_model, map_location=device)
     model_dict = model.state_dict()
     pretrained_dict = {}
     for key, val in pt_dict.items():
@@ -66,21 +66,27 @@ def main():
         A.Rotate(p=0.5, limit=20, border_mode=cv2.BORDER_CONSTANT, value=0),
         A.GridDistortion(p=0.5, num_steps=3, distort_limit=0.3, border_mode=4, interpolation=1),
         A.RandomScale(),
-        A.Resize(512, 512),
+        A.Resize(270, 270),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(
             0.229, 0.224, 0.225), max_pixel_value=1),
-        ToTensor()
+        ToTensorV2(transpose_mask=True)
     ])
     #* Normalise to ImageNet mean and std 
     valid_transforms = A.Compose(
-        [A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=1), ToTensor()])
+        [A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), 
+        max_pixel_value=1), 
+        ToTensorV2(transpose_mask=True)])
 
     #~ init. Datasets
     train_dataset = customDataset(train_path, train_transforms, read_masks=True, 
-                window=config['TRAINING'].getint('Window'), level=config['TRAINING'].getint('Level'))
+            normalise=config['TRAINING'].getboolean('Normalise'),
+            window=config['TRAINING'].getint('Window'), level=config['TRAINING'].getint('Level'))
+
     valid_dataset = customDataset(
         valid_path, valid_transforms, read_masks=True, 
+        normalise = config['TRAINING'].getboolean('Normalise'),
         window=config['TRAINING'].getint('Window'), level=config['TRAINING'].getint('Level'))
+
     train_loader = DataLoader(train_dataset, batch_size)
     valid_loader = DataLoader(valid_dataset, batch_size)
 

utils/loops.py
@@ -6,8 +6,10 @@ import torch
 import torch.nn as nn
 
 from tqdm import tqdm
+from torch.cuda.amp import autocast, GradScaler
 
 from utils.customLosses import diceLoss
+from torch.nn.utils import clip_grad_norm_
 
 class segmenter():
     def __init__(self, model, optimizer, train_loader, val_loader, writer, num_epochs, device="cuda:0"):
@@ -21,6 +23,7 @@ class segmenter():
         self.val_loader = val_loader
         self.writer = writer
         self.num_epochs = num_epochs
+        self.scaler = GradScaler()
         self.best_loss = 10000
         self.output_path = './logs/'
         self.weights = (1, 1) #* X-ent weight vs DSC weight
@@ -32,28 +35,35 @@ class segmenter():
             self.training(epoch)
             self.validation(epoch)
             self.save_best_model()
-
-
+    
     def training(self, epoch, writer_step=25):
         self.model.train()
         self.writer.reset_losses()
         for idx, data in enumerate(tqdm(self.train_loader)):
             inputs = data['inputs'].to(self.device, dtype=torch.float32)
             targets = data['targets'].to(self.device, dtype=torch.float32)
-            # Zero parameter gradient
+            
+            #* Zero parameter gradient
             self.optimizer.zero_grad()
-            outputs = self.model(inputs)
+            with autocast():
+                outputs = self.model(inputs)
 
-            bce_loss = self.bce(outputs['out'], targets)
-            dice_loss = self.dsc(outputs['out'], targets)
-            train_loss = self.weights[0]*bce_loss + self.weights[1]*dice_loss
-
+                bce_loss = self.bce(outputs['out'], targets)
+                dice_loss = self.dsc(outputs['out'], targets)
+                train_loss = self.weights[0]*bce_loss + self.weights[1]*dice_loss
+            
+            #* Compute backward pass on scaled loss
+            self.scaler.scale(train_loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            
             self.writer.metrics['train_loss'].append(train_loss.item())
-            train_loss.backward()
-            self.optimizer.step()
+        
             if epoch % writer_step == 0 and idx == 0:
                 print('Plotting inputs...')
                 self.writer.plot_inputs('Inputs', inputs)
+                self.writer.plot_segmentation(
+                    'Predictions', inputs, outputs['out'], targets=targets)
 
         print('Train Loss:', np.mean(self.writer.metrics['train_loss']))
         self.writer.add_scalar('Training_loss', np.mean(self.writer.metrics['train_loss']), epoch)